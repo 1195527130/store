6369c6c94f05e41be96dc7b58eceadba9bf63981
fuyi <a1195527130@163.com>
2021-05-25 16:09:04 +0800

    衣服商城.py

--- 衣服商城.py
+++ 衣服商城.py
@@ -1,9 +1,34 @@
+id1 = 1
+name1 = "牛仔裤"
+price1 = 88
+num1 = 30
+produce1 = "优衣库"
+
+id2 = 2
+name2 = "衬衫"
+price2 = 66
+num2 = 30
+produce2 = "优衣库"
+
+id3 = 3
+name3 = "T恤"
+price3 = 68
+num3 = 50
+produce3 = "古驰"
+
+id4 = 4
+name4 = "羽绒服"
+price4 = 89
+num4 = 50
+produce4 = "海澜之家"
+
+
 print("==============欢迎来到符懿衣服商城=================")
 print("-----------------------------------------------")
 print("编号       名称      价格      数量      品牌      ")
-print("01        牛仔裤     88       30      优衣库    ")
-print("02        衬衫       66       30      优衣库    ")
-print("03        T恤       68       50       古驰     ")
-print("04        羽绒服     89       50       海澜之家  ")
+print(id1,"       ",name1,"   ",price1,"     ",num1,"    ",produce1)
+print(id2,"       ",name2,"     ",price2,"     ",num2,"    ",produce2)
+print(id3,"       ",name3,"      ",price3,"     ",num3,"    ",produce3)
+print(id4,"       ",name4,"   ",price4,"     ",num4,"    ",produce4)
 print("-----------------------------------------------")
-print("总价格：",(88 * 30 + 66 * 30 + 68 * 50 + 89 * 50),"元")
+print("总价格：",(price1 * num1 + price2 * num2 + price3 * num3 + price4 * num4),"元")
