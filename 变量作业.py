903883f24cff88bb4c87a43ea4977c41e7c09f6d
fuyi <a1195527130@163.com>
2021-05-26 10:07:09 +0800

    day02作业

---
+++ 变量作业.py
@@ -0,0 +1,21 @@
+print("hello world")
+print("-----我是一个分隔符-----")
+print("6+9=",(6+9))
+print("-----我是一个分隔符-----")
+stu1 = 45
+stu2 = 23
+print("stu1+stu2=",(stu1 + stu2))
+print("-----我是一个分隔符-----")
+num1 = 5
+num2 = 6
+num3 = 9
+print("num1+num2+num3=",(num1 + num2 + num3))
+print("-----我是一个分隔符-----")
+num1 = 45
+num2 = num1
+print("num2=",num2)
+print("-----我是一个分隔符-----")
+chromeDriver = "谷歌驱动"
+gekodriver = "火狐驱动"
+print("chromeDriver：",chromeDriver)
+print("gekodriver：",gekodriver)
