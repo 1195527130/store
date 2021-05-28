47c094d992d88c77dd4814dcb36c892d0a38dfe3
fuyi <a1195527130@163.com>
2021-05-28 10:34:16 +0800

    修改

--- 列表翻转.py
+++ 列表翻转.py
@@ -1,3 +1,9 @@
 a = [1,2,3,4,5,6,7,8,9]
-a = list(reversed(a))
+# a = list(reversed(a))
+# print(a)
+for i in range(0,len(a)//2) :
+    b = a[i]
+    a[i] = a[len(a)-i-1]
+    a[len(a)-1-i] = b
+    i = i + 1
 print(a)
